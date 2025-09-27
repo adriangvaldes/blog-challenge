e49b9159f7f7d8d971df0d8875d0c37ef5a4e3d6
Adrian Valdes <adriangvaldes@gmail.com>
2025-09-26 23:43:47 -0300

    Fix post messages to use English and improve error messages in post handlers

--- handlers/post_handlers.go
+++ handlers/post_handlers.go
@@ -11,7 +11,7 @@ import (
 )
 
 var posts = []models.Post{
-	{ID: 1, Title: "Meu Primeiro Post", Content: "Olá, mundo!", AuthorID: 1, CreatedAt: time.Now()},
+	{ID: 1, Title: "My first post", Content: "Hello, world!", AuthorID: 1, CreatedAt: time.Now()},
 }
 
 func GetPosts(w http.ResponseWriter, r *http.Request) {
@@ -55,5 +55,54 @@ func GetPost(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	http.Error(w, "Post não encontrado", http.StatusNotFound)
+	http.Error(w, "Post not found", http.StatusNotFound)
+}
+
+func DeletePost(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idStr := vars["id"]
+
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		http.Error(w, "ID invalid!", http.StatusBadRequest)
+		return
+	}
+
+	for i, post := range posts {
+		if post.ID == id {
+			posts = append(posts[:i], posts[i+1:]...)
+
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+	}
+}
+
+func UpdatePost(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	id, err := strconv.ParseInt(vars["id"], 10, 64)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	var updatedPost models.Post
+	if err := json.NewDecoder(r.Body).Decode(&updatedPost); err != nil {
+		http.Error(w, "Invalid Data", http.StatusBadRequest)
+		return
+	}
+
+	for i, p := range posts {
+		if p.ID == id {
+			posts[i].Title = updatedPost.Title
+			posts[i].Content = updatedPost.Content
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(posts[i])
+			return
+		}
+	}
+
+	http.Error(w, "Post not found", http.StatusNotFound)
 }
